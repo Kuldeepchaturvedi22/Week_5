7dbadbe728c7be87938092dbe930f875274b4538
Kuldeepchaturvedi22 <chaturvedikuldeep40@gmail.com>
2025-02-20 10:24:49 +0530

    [Kuldeep] Add. Added program of Convert a Java object (Car) into JSON format.

---
+++ day_2/src/main/com/practiceproblem/convertjavaobjectintojson/Car.java
@@ -0,0 +1,19 @@
+package com.practiceproblem.convertjavaobjectintojson;
+
+public class Car {
+    // Attributes of the Car class
+    public String name;
+    public String color;
+    public double price;
+    public String type;
+    public String brand;
+
+    // Constructor to initialize the Car object with specified attributes
+    public Car(String name, String color, double price, String type, String brand) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+        this.type = type;
+        this.brand = brand;
+    }
+}
